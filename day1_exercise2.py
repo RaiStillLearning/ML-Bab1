28cdd075823257fb9903765b7c932e03a20fa862
RaiStillLearning <rakhaarkhana98@gmail.com>
2025-08-13 13:33:47 +0700

    pengenalan variables dan tipe data dasar

---
+++ day1_exercise2.py
@@ -0,0 +1,23 @@
+print("Hello, AI World")
+
+#define variables of different data types types
+integer_var = 42
+float_var = 3.14
+string_var = "Hello, World"
+list_var = [1, 2, 3, 4, 5]
+tuple_var = (10, 11, 12)
+dict_var = {"Name": "Raka", "role": "Engineer"}
+boolean_var = False
+boolean_var2 = True
+
+
+#print and manipulate variables
+print("integer ", integer_var)
+print("float ", float_var)
+print("string ", string_var + "Ai Bootcamp")
+list_var.append(6)  # adding an element to the list
+print("list ", list_var)
+print("tuple ", tuple_var)
+print("dictionary ", dict_var["Name"])
+print("boolean ", boolean_var)
+print("boolean2 ", boolean_var2)
