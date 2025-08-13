21b83f0ae91339dc817303bcc7385b2a5d7f9e47
RaiStillLearning <rakhaarkhana98@gmail.com>
2025-08-13 13:23:54 +0700

    pengenalan data python basic

---
+++ pengenalan_data.py
@@ -0,0 +1,22 @@
+print("Hello, PYTHON")
+
+# contoh integers dan floats
+age = 25
+height = 5.9
+
+
+# strings
+name = "John Doe"
+greeting = "Hello, " + name
+# lists
+fruits = ["apple", "banana", "cherry"]
+# tuples
+coordinates = (10.0, 20.0)
+# dictionaries
+person = {
+    "name": "Alice",
+    "age": 30,
+    "city": "New York"
+}
+#booleans
+is_student = True
