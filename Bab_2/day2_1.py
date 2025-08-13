7354b79864a09b31a3d136f32ad73e7f619d1a0c
RaiStillLearning <rakhaarkhana98@gmail.com>
2025-08-13 13:52:08 +0700

    belajar statement conditional dan nested conditional

---
+++ Bab_2/day2_1.py
@@ -0,0 +1,17 @@
+num = -10
+if num > 5:
+    print("Number is greater than 5")
+elif num == 5:
+    print("Number is equal to 5")
+else:
+    print("Number is less than 5")
+print("End of conditional checks")
+
+# contoh 2: nested conditional statements
+age = 18
+if age >= 18:
+    if age <= 30:
+        print("Young Adult")
+else:
+    print("Adult")
+    
